a3252570e530ad0b4d888adc7eac0e18314cd5ad
mouyong <my242551325@gmail.com>
2021-07-28 16:23:01 +0800

    feat: 添加 cron

--- docker-php-entrypoint.sh
+++ docker-php-entrypoint.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
 set -ex
 /usr/bin/python /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
-
-
-if [ -f /var/www/html/crontab ]; then
-  crontab /var/www/html/crontab
-fi
-
-crontab -l
-
-[ -f /var/www/html/crontab ]
-
-echo $?
-
-
 docker-run
