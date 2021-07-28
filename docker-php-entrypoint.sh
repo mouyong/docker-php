1ce78ad6435677d57cfc2554ea1ca2f9c808fec5
mouyong <my242551325@gmail.com>
2021-07-28 16:30:51 +0800

    feat: 调整 cron

--- docker-php-entrypoint.sh
+++ docker-php-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 set -ex
 /usr/bin/python /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+cron -f &
+if [ -f /var/www/html/crontab ]; then
+  crontab /var/www/html/crontab
+fi;
 docker-run
