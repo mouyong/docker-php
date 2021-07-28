7d35e2e26c5e8f118320c48a6198e7a05be64d6f
mouyong <my242551325@gmail.com>
2021-07-28 16:40:34 +0800

    feat: 调整 cron

--- docker-php-entrypoint.sh
+++ docker-php-entrypoint.sh
@@ -2,8 +2,3 @@
 set -ex
 /usr/bin/python /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 docker-run
-
-cron -f &
-if [ -f /var/www/html/crontab ]; then
-  crontab /var/www/html/crontab
-fi;
--- fpm/docker-run-fpm.sh
+++ fpm/docker-run-fpm.sh
@@ -6,3 +6,8 @@
 /usr/local/bin/php /var/www/html/artisan route:cache
 # 缓存配置
 /usr/local/bin/php /var/www/html/artisan config:cache
+
+if [ -f /var/www/html/crontab ]; then
+  crontab /var/www/html/crontab
+fi;
+cron -f &
