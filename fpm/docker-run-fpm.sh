06b5a14fe5077f2505432652c72ef586f33e22e3
mouyong <my242551325@gmail.com>
2021-07-28 10:22:58 +0800

    feat: 添加 cron

--- fpm/docker-run-fpm.sh
+++ fpm/docker-run-fpm.sh
@@ -6,3 +6,10 @@
 /usr/local/bin/php /var/www/html/artisan route:cache
 # 缓存配置
 /usr/local/bin/php /var/www/html/artisan config:cache
+
+echo "cron"
+mkdir -p /var/spool/cron/crontabs/
+cp crontab /var/spool/cron/crontabs/root
+chmod 0600 /var/spool/cron/crontabs/root
+crontab /var/spool/cron/crontabs/root
+cron -f &
