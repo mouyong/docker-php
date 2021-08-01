d311a0efafcc48595d0d0eb2a389f58b1c555b82
mouyong <my242551325@gmail.com>
2021-08-01 16:07:59 +0800

    feat: 调整定时任务

--- docker-php-entrypoint.sh
+++ docker-php-entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -ex
+cron
 /usr/bin/python /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 su - www-data -s /bin/bash -c "docker-run"
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
+fi
+exec "$@"
