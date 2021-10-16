89cebf0d622c59319a6b879e11e03f7960e87e3a
mouyong <my242551325@gmail.com>
2021-10-16 12:03:48 +0800

    feat: 更新部署

--- docker-php-entrypoint.sh
+++ docker-php-entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 set -ex
+
+case $PLATFORM in \
+"serverless") \
+  echo "run in $PLATFORM" \
+	mv /etc/supervisor/conf.d/queue.conf queue.conf.back && \
+	mv /var/www/html/crontab crontab.back\
+;; \
+"vpc") \
+    echo "run in $PLATFORM" \
+;; \
+"*") \
+  echo "unknown platform $PLATFORM" \
+;;
+esac
+
 cron
 /usr/bin/python /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 su - www-data -s /bin/bash -c "docker-run"
